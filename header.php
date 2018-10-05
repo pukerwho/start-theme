8b14dfb4d06ae2605e9a9c54e460d13c3e95abba
Vladimir Samojlov <v-moi@ya.ru>
2018-10-05 21:34:48 +0300

    add screenshot and google analitycs

--- header.php
+++ header.php
@@ -13,6 +13,16 @@
 
     wp_head();
   ?>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-127078049-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-127078049-1');
+  </script>
+
 </head>
 <body <?php echo body_class(); ?>>
   <!-- <div class="preloader"></div> -->
