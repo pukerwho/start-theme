46a29deb44a88ecf9f0646e70bf1feb1f88cdb0f
Vladimir Samojlov <v-moi@ya.ru>
2018-10-08 16:17:51 +0300

    add jivosite

--- footer.php
+++ footer.php
@@ -3,5 +3,10 @@
     	
     </footer>
     <?php wp_footer(); ?>
+    <!-- BEGIN JIVOSITE CODE {literal} -->
+		<script type='text/javascript'>
+		(function(){ var widget_id = 'jZM8fted29';var d=document;var w=window;function l(){var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+		</script>
+		<!-- {/literal} END JIVOSITE CODE -->
 </body>
 </html>
