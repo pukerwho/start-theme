08bd2cebd4d13c1dd90249f6005f2b03b07bbe7c
Vladimir Samojlov <v-moi@ya.ru>
2018-10-09 13:08:02 +0300

    fixed header

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,12 @@ $('.toogle-menu').on('click', function(e) {
   $('.slide-menu').toggleClass('slide-menu_active');
   $('.menu li').toggleClass('animate-left');
 });
+
+$(window).scroll(function(){
+  var h_scroll = $(this).scrollTop();
+  if (h_scroll > 56) {
+    $('header').addClass('header__fixed')
+  } else {
+    $('header').removeClass('header__fixed')
+  }
+})
