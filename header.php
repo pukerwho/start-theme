fb50b1c917f0ac98708f338abb80521115be55d2
Vladimir Samojlov <v-moi@ya.ru>
2020-07-02 19:19:52 +0300

    edit

--- header.php
+++ header.php
@@ -7,6 +7,7 @@
 
   <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
   <link rel="profile" href="http://gmpg.org/xfn/11">
+  <link rel="apple-touch-icon" href="/example.png">
   <base href="/">
   <link rel="alternate" hreflang="x-default" href="<?php echo home_url(); ?>">
   <link rel="alternate" hreflang="en" href="<?php echo home_url(); ?>/en">
